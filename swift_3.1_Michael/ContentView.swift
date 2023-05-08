e14a6d66977e775c101962a02365ea4ac6a7920f
michaelschwentner <mid.schwentner@gmail.com>
2023-05-08 11:52:26 +0200

    Initial Commit

---
+++ swift_3.1_Michael/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swift_3.1_Michael
+//
+//  Created by Michael Schwentner on 08.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_3.1_Michael/swift_3_1_MichaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_3_1_MichaelApp.swift
+//  swift_3.1_Michael
+//
+//  Created by Michael Schwentner on 08.05.23.
+//
+
+import SwiftUI
+
+@main
+struct swift_3_1_MichaelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
